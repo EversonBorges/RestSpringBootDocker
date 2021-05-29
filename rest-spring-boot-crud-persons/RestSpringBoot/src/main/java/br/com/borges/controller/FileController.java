1647f32cf8d1150479ba03197b1e5ab015ab286f
Everson Borges <everson.cursos@gmail.com>
2021-05-29 15:51:47 -0300

    Trabalahndo com Docker

--- rest-spring-boot-crud-persons/RestSpringBoot/src/main/java/br/com/borges/controller/FileController.java
+++ rest-spring-boot-crud-persons/RestSpringBoot/src/main/java/br/com/borges/controller/FileController.java
@@ -4,8 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +30,8 @@ import io.swagger.annotations.Api;
 @RestController
 @RequestMapping("/api/file")
 public class FileController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 	
 	@Autowired
 	private FileStorageService service;
@@ -43,5 +54,27 @@ public class FileController {
 				.stream()
 				.map(file -> uploadFile(file))
 				.collect(Collectors.toList());
+	}
+	
+	@GetMapping("/downloadFile/{fileName:.+}")
+	public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request){
+		
+		Resource resource = service.loadFileAsResource(fileName);
+		
+		String contentType = null;
+		
+		try {
+			contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+		} catch (Exception e) {
+			logger.info("Não consegui determinar o tipo de arquivo!");
+		}
+		
+		if(contentType == null) {
+			contentType = "application/octet-stream";
+		}
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; fileName=\"" + resource.getFilename() + "\"")
+				.body(resource);
 	}
 }
--- rest-spring-boot-crud-persons/RestSpringBoot/src/main/java/br/com/borges/services/FileStorageService.java
+++ rest-spring-boot-crud-persons/RestSpringBoot/src/main/java/br/com/borges/services/FileStorageService.java
@@ -6,43 +6,64 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.borges.config.FileStorageConfig;
 import br.com.borges.exception.FileStorageException;
+import br.com.borges.exception.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
 
 	private final Path fileStorageLocation;
-	
+
 	@Autowired
 	public FileStorageService(FileStorageConfig fileStorageConfig) {
 		this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
-		
+
 		try {
 			Files.createDirectories(this.fileStorageLocation);
 		} catch (Exception e) {
-			throw new FileStorageException("Não foi possivel criar o diretório em que os arquivos serão armazenados",e);
+			throw new FileStorageException("Não foi possivel criar o diretório em que os arquivos serão armazenados",
+					e);
 		}
 	}
-	
+
 	public String storeFile(MultipartFile file) {
 		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-		
+
 		try {
 			if (fileName.contains("..")) {
 				throw new FileStorageException("Desculpe, O nome do arquivo contem seguencia invalida " + fileName);
 			}
-			
+
 			Path targetLocation = this.fileStorageLocation.resolve(fileName);
 			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-			
+
 			return fileName;
 		} catch (Exception e) {
-			throw new FileStorageException("Não foi possivel criar o diretório "+ fileName + " por favor tente novamente!",e);
+			throw new FileStorageException(
+					"Não foi possivel criar o diretório " + fileName + " por favor tente novamente!", e);
+		}
+	}
+
+	public Resource loadFileAsResource(String fileName) {
+
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) {
+				return resource;
+			}else {
+				throw new MyFileNotFoundException("Arquivo não encontrado " + fileName);
+			}
+
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("Arquivo não encontrado " + fileName, e);
 		}
 	}
 
